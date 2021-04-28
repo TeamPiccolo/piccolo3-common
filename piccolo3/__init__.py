fadf27385af242e8a67ee4af1ba80031f92d6386
mhagdorn <magnus.hagdorn@marsupium.org>
2021-04-28 11:16:19 +0100

    fix license stuff

--- piccolo3/__init__.py
+++ piccolo3/__init__.py
@@ -1,19 +1,19 @@
 # Copyright 2018- The Piccolo Team
 #
-# This file is part of piccolo3-server.
+# This file is part of piccolo3-common.
 #
-# piccolo3-server is free software: you can redistribute it and/or modify
+# piccolo3-common is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# piccolo3-server is distributed in the hope that it will be useful,
+# piccolo3-common is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with piccolo3-server.  If not, see <http://www.gnu.org/licenses/>.
+# along with piccolo3-common.  If not, see <http://www.gnu.org/licenses/>.
 
 try:
     __import__('pkg_resources').declare_namespace(__name__)
